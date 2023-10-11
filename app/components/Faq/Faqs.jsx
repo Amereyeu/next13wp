0bdca16a9506ddb3f93147c4fb37e8f3d9fbd85b
Amerey <info@amerey.eu>
2023-10-11 08:32:21 +0200

    next13 + wp

---
+++ app/components/Faq/Faqs.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+
+import { useState } from "react";
+import SingleFaq from "./SingleFaq";
+import data from "@/data/data.json";
+
+function Faqs({ lg }) {
+  const [faqs, setfaqs] = useState(data);
+
+  const toggleFAQ = (index) => {
+    setfaqs(
+      faqs.map((faq, i) => {
+        if (i === index) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
+
+  return (
+    <div className="faqs">
+      {faqs.map((faq, i) => (
+        <SingleFaq faq={faq} index={i} toggleFAQ={toggleFAQ} key={i} lg={lg} />
+      ))}
+    </div>
+  );
+}
+
+export default Faqs;
+
